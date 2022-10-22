8e8ff32b5d722be2092afe0e8270b18bd0e097d7
Junior <dirceusilva0422@gmail.com>
2022-10-22 16:20:31 -0300

    test new functions

---
+++ bank/src/generateCard.js
@@ -0,0 +1,26 @@
+function generateCard(){
+    const number1 = Math.floor(Math.random() * 10)
+    const number2 = Math.floor(Math.random() * 10)
+    const number3 = Math.floor(Math.random() * 10)
+    const number4 = Math.floor(Math.random() * 10)
+    const number5 = Math.floor(Math.random() * 10)
+    const number6 = Math.floor(Math.random() * 10)
+    const number7 = Math.floor(Math.random() * 10)
+    const number8 = Math.floor(Math.random() * 10)
+    const number9 = Math.floor(Math.random() * 10)
+    const number10 = Math.floor(Math.random() * 10)
+    const number11 = Math.floor(Math.random() * 10)
+    const number12 = Math.floor(Math.random() * 10)
+    const number13 = Math.floor(Math.random() * 10)
+    const number14 = Math.floor(Math.random() * 10)
+    const number15 = Math.floor(Math.random() * 10)
+    const number16 = Math.floor(Math.random() * 10)
+    var 
+    cc = [number1, number2, number3, number4, 
+      number5, number6, number7, number8, number9, 
+      number10, number11, number12, number13, number14, number15, number16]
+    var newCc = cc.toString()
+    console.log(newCc)
+    }
+  
+  generateCard()
