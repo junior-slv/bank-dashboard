8c6397bdb49cbf47a1cfa92876cb7ef21d952c01
junior-slv <dirceusilva0422@gmail.com>
2022-10-24 19:52:37 -0300

    Added main page login screen

--- bank/src/index.js
+++ bank/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import MainPage from './pages/MainPage';
 import Menu from './pages/Menu';
 
 
@@ -9,6 +10,7 @@ import Menu from './pages/Menu';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Menu />
+    <MainPage/>
+    <Menu/>
   </React.StrictMode>
 );
---
+++ bank/src/pages/MainPage.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './MainPage.css';
+
+
+
+
+function MainPage() {
+  return (
+    <div className="container">
+      <div className='mainMenu'>
+            <div className='mainMenuText'>
+                <h1>Robby bank</h1>
+            </div>
+            <div className='login'>
+                <p>Login</p>
+                <input type="email">
+                </input>
+            </div>
+            <div className='password'>
+                <p>Password</p>
+                <input type="password">
+                </input>
+            </div>
+      </div>
+    </div>
+  );
+}
+
+export default MainPage;
+ 
+
+
+
