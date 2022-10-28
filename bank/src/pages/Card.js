1a7f6dc4079830436acbfbdb33683863c2e24c58
junior-slv <dirceusilva0422@gmail.com>
2022-10-28 15:58:01 -0300

    New css features

--- bank/src/pages/Card.js
+++ bank/src/pages/Card.js
@@ -9,20 +9,38 @@ import { newCc1, newCc2,newCc3, newCc4} from '../scripts/generateCard.js';
 function Card() {
   return (
     <div className='container'>
+      <h1>Enjoy your new Robby Card :)</h1>
       <div className='card'>
-        <div className='cardNumber'>
-          <span ><p className='CardNumbers' id='serialOne'>{newCc1}</p></span>
-          <span ><p className='CardNumbers' id='serialTwo'>{newCc2}</p></span>
-          <span ><p className='CardNumbers' id='serialThree'>{newCc3}</p></span>
-          <span ><p className='CardNumbers' id='serialFour'>{newCc4}</p></span>
+        <div class="card__front card__part">
+          <p class="card_numer"><p className='CardNumbers' id='serialOne'>{newCc1}</p>
+          <p className='CardNumbers' id='serialTwo'>{newCc2}</p>
+          <p className='CardNumbers' id='serialThree'>{newCc3}</p>
+          <p className='CardNumbers' id='serialFour'>{newCc4}</p>
+          </p>
+          <div class="card__space-75">
+            <span class="card__label">Card holder</span>
+            <p class="card__info">Juninho Silva</p>
+          </div>
+          <div class="card__space-25">
+            <span class="card__label">Expires</span>
+                  <p class="card__info">10/25</p>
+          </div>
         </div>
-        <div className='cardExpiration'><p>04/32</p></div>
-        <div className='cardName'>Maria Luiza Gonçalves Pereira</div>
+        
+        <div class="card__back card__part">
+          <div class="card__black-line"></div>
+          <div class="card__back-content">
+            <div class="card__secret">
+              <p class="card__secret--last">420</p>
+            </div>
+            
+          </div>
+        </div>
+        
       </div>
     </div>
   );
 }
-// {newCc1, newCc2, newCc3, newCc4}
 export default Card;
  
 
