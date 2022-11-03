b86b34529cfbf7b87da94976a4900ba5ce9e2d70
Junior <dirceusilva0422@gmail.com>
2022-11-03 10:56:06 -0300

    test: Testing api

--- bank/src/scripts/UserService.js
+++ bank/src/scripts/UserService.js
@@ -1,39 +1,12 @@
 import axios from 'axios';
 
-export default class UserServices {
-  constructor () {
-    this.axios = axios.create({
-      baseURL: process.env.REACT_APP_API_LOGIN + '/api' 
-    })
-  }
+const url = "https://robby-bank.herokuapp.com/users"
 
-  async login (dados) {
-    const {data} = await this.axios.post('/login', dados)
-
-    if (data) {
-      localStorage.setItem("nome", data.users.nome)
-      localStorage.setItem("email", data.users.email)
-      localStorage.setItem("token", data.token.token)
-
-      return true
-    }
-
-    return
-  }
-
-  async cadastrar (dados) {
-    return this.axios.post('/user', dados)
-  }
-
-  usuarioAutenticado () {
-    return localStorage.getItem("token") != undefined ? true : false
-    // return typeof localStorage.getItem("token")
-  }
-
-  //Desafio ---> implemente um botão que chama essa função dentro da página Home
-  async logout () {
-    localStorage.removeItem("token")
-    localStorage.removeItem("nome")
-    localStorage.removeItem("email")
-  }
+function getUser(){
+  axios.get(url)
+  .then(response =>{
+    console.log(response)
+  })
+  .catch(error => console.log(error))
 }
+getUser()
